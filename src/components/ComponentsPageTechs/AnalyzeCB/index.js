af08aba4b12a33b38a5c278300727ad495c5c784
Messias <messias.souza@academico.ifpb.edu.br>
2020-11-09 11:25:22 -0300

    estilizaçao dos componentes AnalyzeCB, AnalyzeCO e Mapa

--- src/components/ComponentsPageTechs/AnalyzeCB/index.js
+++ src/components/ComponentsPageTechs/AnalyzeCB/index.js
@@ -4,8 +4,50 @@ import './index.css';
 function AnalyzeCB() {
     return (
         <div className='analyze-cb'>
-            <div className='ana-cb'>
-
+            <div className='casca-ana-cb'>
+                <div className='cb-label'>ANÁLISE CUSTO BENEFÍCIO</div>
+                <div className='cb-graphic'>
+                    <div className='cus-ins'>
+                        <div className='c-i-gra'>R$0</div>
+                        <div className='cb-gra-label'>Custo de instalação</div>
+                    </div>
+                    <div className='cus-man'>
+                        <div className='c-m-gra'>R$0</div>
+                        <div className='cb-gra-label'>Custo de manutenção por ano</div>
+                    </div>
+                    <div className='tem-vid'>
+                        <div className='t-m-gra'>0 anos</div>
+                        <div className='cb-gra-label'>Tempo de vida útil da tecnologia</div>
+                    </div>
+                    <div className='imp-amb'>
+                        <div className='i-m-gra'>0%</div>
+                        <div className='cb-gra-label'>Impactos ambientais</div>
+                    </div>
+                </div>
+                <div className='cb-bottom'>
+                    <div className='txt-left'>
+                        <div className='txt-label'> IMPACTOS SOCIAIS</div>
+                        <div className='cb-txt'>
+                            Lorem ipsum dolor sit amet consectetur adipiscing
+                            elit natoque phasellus congue, dis blandit id aliquet
+                            morbi felis litora iaculis. Nisl sociosqu justo phasellus
+                            vitae risus nunc vel mattis eu, ultrices feugiat malesuada
+                            eleifend dui pretium duis posuere nascetur,
+                            faucibus sapien natoque velit dis quis ex tempor.
+                        </div>
+                    </div>
+                    <div className='txt-right'>
+                        <div className='txt-label'>ADAPTAÇÃO Á REALIDADE DA AGRICULTURA FAMILIAR</div>
+                        <div className='cb-txt'>
+                            Lorem ipsum dolor sit amet consectetur adipiscing
+                            elit natoque phasellus congue, dis blandit id aliquet
+                            morbi felis litora iaculis. Nisl sociosqu justo phasellus
+                            vitae risus nunc vel mattis eu, ultrices feugiat malesuada
+                            eleifend dui pretium duis posuere nascetur,
+                            faucibus sapien natoque velit dis quis ex tempor.
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/ComponentsPageTechs/AnalyzeCO/index.js
+++ src/components/ComponentsPageTechs/AnalyzeCO/index.js
@@ -2,11 +2,55 @@ import React from 'react';
 import './index.css';
 
 function AnalyzeCO() {
-    return(
-        <div className='analyze-co'> 
-            
+    return (
+        <div className='analyze-co'>
+            <div className='casca-ana-co'>
+                <div className='co-label'>ANÁLISE DE EMISSÃO DE CO2 (-245KG/MÊS)</div>
+                <div className='co-graphic'>
+                    <div className='gra-label'>QUANTIDADE INSTALADA E NÚMERO DE BENEFICIÁRIOS</div>
+                    <div className='casca-gra'>
+                        <div className='graphic'>
+                            <div className='label'>
+                                <div className='left'>QUANTIDADE INSTALADA</div>
+                                <div className='right'>12</div>
+                            </div>
+                            <div className='line-gra'>
+                                <div></div>
+                            </div>
+                        </div>
+                        <div className='graphic'>
+                            <div className='label'>
+                                <div className='left'>NUMERO DE BENEFICIADOS</div>
+                                <div className='right'>2.492</div>
+                            </div>
+                            <div className='line-gra'>
+                                <div></div>
+                            </div>
+                        </div>
+                        <div className='graphic'>
+                            <div className='label'>
+                                <div className='left'>MUNICÍPIOS</div>
+                                <div className='right'>234</div>
+                            </div>
+                            <div className='line-gra'>
+                                <div></div>
+                            </div>
+                        </div>
+                        <div className='graphic'>
+                            <div className='label'>
+                                <div className='left'> ************** </div>
+                                <div className='right'> **** </div>
+                            </div>
+                            <div className='line-gra'>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='co-bottom'></div>
+            </div>
         </div>
-        )
+    )
 }
- 
-export default AnalyzeCO;
+
+export default AnalyzeCO; 
