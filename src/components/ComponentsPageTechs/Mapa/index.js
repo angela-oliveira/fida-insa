b05b44737c6fdf8db0e126fc197c72cbefbc062a
Messias <messias.souza@academico.ifpb.edu.br>
2020-11-21 15:24:56 -0300

    implementaçao grafica do mapa

--- src/components/ComponentsPageTechs/Mapa/index.js
+++ src/components/ComponentsPageTechs/Mapa/index.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { select, geoPath, geoMercator } from "d3";
+import useResizeObserver from 'use-resize-observer';
+import data from '../../../utils/filesJSON/semiarido.geo.json'
+
 import './index.css';
 
 function Mapa() {
+
+    // referencia os elementos
+    const svgRef = useRef();
+    const wrapperRef = useRef(); 
+
+    // adquirindo a proporção dada pelo css
+    const dimensions = useResizeObserver(wrapperRef);
+    console.log(dimensions)
+
+    const [selectedEstados, setSelectedEstados] = useState(); 
+
+    useEffect(() => {
+
+
+        const svg = select(svgRef.current);
+
+        const { width, height } = dimensions || wrapperRef.current.getBoundingClientRects();
+        console.log()
+
+
+        const projection = geoMercator().fitSize([400, 620], selectedEstados || data);
+        console.log(projection)
+
+        const pathGenerator = geoPath().projection(projection);
+        console.log(pathGenerator)
+        svg
+            .selectAll(".estado")
+            .data(data.features)
+            .join("path")
+            .on("click", features => {
+                setSelectedEstados(features);
+            })
+            .attr("class", "estado")
+            .transition()
+            .attr("d", features => pathGenerator(features));
+        }, [data, dimensions])
+
+        
     return (
         <div className='map-container'>
             <div className='casca-map'>
@@ -9,7 +51,7 @@ function Mapa() {
 
                     <div className='title-map'>MAPA DO SEMIÁRIDO</div>
                     <div className='info-text'>
-                        <div className='txt-title'>COMUNIDADE RIBEIRINHA</div>
+                        <div className='txt-title'>COMUNIDADE DE RIBEIRINHA</div>
                         <div className='txt-sub'>Cabaceira, Paraiba</div>
                         <div className='txt'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -22,10 +64,14 @@ function Mapa() {
                             auctor quam.
                         </div>
                     </div>
-                    <div className='info-media'></div>
+                    <div className='info-media'>
+                        <b>MÍDIA EM BREVE! 😉</b>
+                    </div>
 
                 </div>
-                <div className='map'></div>
+                <div className='map' ref={wrapperRef}>
+                    <svg ref={svgRef}></svg>
+                </div>
             </div>
         </div>
     )
