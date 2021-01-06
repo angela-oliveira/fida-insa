75be2e74bc020394ef4b5f4ed92cef81a60b8b77
angela.oliveira <angela.oliveira@assert.ifpb.edu.br>
2021-01-05 22:51:36 -0300

    curiosidades

--- src/components/ComponentsPageTechs/Curiosities/index.js
+++ src/components/ComponentsPageTechs/Curiosities/index.js
@@ -6,13 +6,9 @@ import Parceiros from "../../../components/Parceiros"
 
 const { Panel } = Collapse;
 
-const text1 = `
-A origem do catavento não está claramente sinalizada na história.
-Mas alguns estudiosos acreditam ter originado na Pérsia de 915 a.C, hoje Irã, os mais antigos registros sobre moinhos de vento. Entretanto, existem indicações sobre o emprego de moinhos de vento mais remotos no Iraque, Egito e China. Na Europa, os moinhos de vento foram introduzidos no século XII, mas apenas no século XV, se espalharam pelo continente. Se prestou a muitas aplicações no passado, como na moagem de grãos, extração de óleo, bombeamento de água, dentre outras (BARROS, 2007).
-`;
-const text2 = `Seu funcionamento baseia-se no movimento da massa de ar, que é o vento, que ao passar pela hélice faz com que suas pás girem. Com o torque existente em seu eixo transferindo a bomba eleva a água ao reservatório (BARROS, 2007).`
-const text3 = `Para bombeamento de água, o cata-vento deve ser instalado em conjunto com
-uma bomba do tipo pistão (BARROS, 2007). Para fins de irrigação, é geralmente utilizado para bombear água para reservatórios posicionados a uma certa altitude. Do reservatório a água pode então ser distribuída por gravidade a sistemas de irrigação por gotejamento, microaspersão ou superficial. É um sistema de alto custo de aquisição em comparação ao sistema de bombeamento elétrico, mas de baixo custo de operação e manutenção. Uma necessidade básica para a instalação do sistema é que na região tenha ventos acima de 2 m/s (EMBRAPA CNPAT, 2004).`
+const text1 = `A origem do catavento remonta aos antigos moinhos de vento. Essa origem não está realmente clara, mas alguns estudiosos acreditam ter originado na Pérsia de 915 a.C, hoje Irã. Entretanto, existem indicações sobre o emprego de moinhos de vento mais remotos no Iraque, Egito e China. Na Europa, os moinhos de vento foram introduzidos no século XII, mas somente no século XV se espalharam pelo continente. Ao longo dos séculos, o uso da energia dos ventos se prestou a muitas aplicações como na moagem de grãos, extração de óleo, bombeamento de água, dentre outras.`
+const text2 = `O funcionamento do catavento baseia-se no movimento das massas de ar, que, ao passar pelas hélices fazem com que suas pás girem. Para bombeamento de água, o catavento deve ser instalado em conjunto com uma bomba do tipo pistão. Para fins de irrigação, é geralmente utilizado para bombear água para reservatórios elevados. Do reservatório, a água pode então ser distribuída por gravidade a sistemas de irrigação por gotejamento, microaspersão ou superfície. É um sistema de alto custo de aquisição em comparação ao sistema de bombeamento elétrico, mas de baixo custo de operação e manutenção.`
+const text3 = `Charles Francis Brush (1849-1929), um dos fundadores da indústria elétrica dos Estados Unidos, construiu um grande moinho de vento na sua propriedade, em Cleveland. Esse foi o primeiro registro de uma turbina movida pelo vento para produção de eletricidade. O diâmetro do rotor era de 17 metros, com 144 lâminas feitas de madeira. A turbina funcionou por 20 anos e era usada para carregar baterias que ficavam no celeiro de sua mansão. Essa primeira versão gerava apenas 12 quilowatts (kW).`
 // const textCuriosities = {
 //     title:"" ,
 //     text: ""
@@ -29,11 +25,13 @@ function Curiosities() {
                 <div className='subtitle'><p>Veja algumas dúvidas e curiosidades</p>
                 </div>
                 <Collapse accordion>
-                    <Panel header="Curiosidades" key="1">
+                    <Panel header="A origem do catavento é datada de 915 a.C" key="1">
                     <p>{text1}</p>
                     </Panel>
-                    <Panel header="Fatos relevantes" key="2">
+                    <Panel header="Uma tecnologia simples com uma fonte energética inesgotável para a agricultura" key="2">
                     <p>{text2}</p>
+                    </Panel>
+                    <Panel header="A transição de um moinho de vento para a geração de energia elétrica" key="3">
                     <p>{text3}</p>
                     </Panel>
                 </Collapse>
