170815c6709f5f9f0d9a4212737a19652391c247
Messias <messias.souza@academico.ifpb.edu.br>
2021-02-05 02:35:56 -0300

    implementaçao do campo fale conosco

--- src/components/ModalContact/index.js
+++ src/components/ModalContact/index.js
@@ -1,44 +1,104 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './index.css';
 import { Button, Modal, Form, Input } from 'antd';
+import { timeout } from 'd3';
 
-const layout = {
-  labelCol: { span: 5 },
-  wrapperCol: { span: 16 },
-};
-const validateMessages = {
-required: '${label} é obrigatório!',
-types: {
-  email: '${label} não é um email válido!',
-},
-};
-const onFinish = (values) => {
-  console.log(values);
-};
+// const layout = {
+//   labelCol: { span: 5 },
+//   wrapperCol: { span: 16 },
+// };
+// const validateMessages = {
+//   required: '${label} é obrigatório!',
+//   types: {
+//     email: '${label} não é um email válido!',
+//   },
+// };
+// const onFinish = (values) => {
+//   console.log(values);
+// };
+
+function validacaoEmail(field) {
+
+  const usuario = field.value.substring(0, field.value.indexOf("@"));
+  const dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
+
+  const erro = document.querySelector('#erro')
+  if ((usuario.length >= 1) &&
+    (dominio.length >= 3) &&
+    (usuario.search("@") == -1) &&
+    (dominio.search("@") == -1) &&
+    (usuario.search(" ") == -1) &&
+    (dominio.search(" ") == -1) &&
+    (dominio.search(".") != -1) &&
+    (dominio.indexOf(".") >= 1) &&
+    (dominio.lastIndexOf(".") < dominio.length - 1)) {
+    document.getElementById("erro").innerHTML = "";
+  }
+  else {
+    document.getElementById("erro").innerHTML = "<font color='red'>Email inválido </font>";
+  }
+}
+
+function validarEmail() {
+  const email = document.querySelector('#email');
+  console.log(email.value)
+
+  validacaoEmail(email)
+
+}
+
+function clearErro() {
+  const erro = document.querySelector('#erro')
+  if (erro.innerHTML == "Email invalido!") {
+    erro.innerHTML = ""
+  }
+}
+
+function direction() {
+  timeout(() => {
+    document.location.reload(true);
+  }, 1000)
+
+}
 
 class ModalContact extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
+      <div>
 
-      <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} size={"large"}>
-        <Form.Item name={['user', 'name']} label="Nome" rules={[{ required: true }]}>
-            <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email', required: true }]}>
-            <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'topic']} label="Assunto">
-            <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'text']} label="Texto">
-            <Input.TextArea />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-            <Button type="primary" htmlType="submit">
-            Enviar
-            </Button>
-        </Form.Item>
-      </Form>
+        <form className='gform' name='feedback' method='POST' action='https://script.google.com/macros/s/AKfycbyw730nRQWuM5F4dTCkYN4th_uDjKeH356BLeKXWg/exec'>
+
+          <div>
+
+            <label for='name'> Nome: </label>
+            <input id='name' name='nome' required ></input>
+          </div>
+
+          <div>
+            <label for='email' > Email: </label>
+            <input id='email' name='email' required onBlur={() => { validarEmail() }}></input>
+
+            <span id='erro'></span>
+          </div>
+          <div>
+            <label for='assunto' > Assunto: </label>
+            <input id='assunto' name='assunto' required></input>
+          </div>
+          <div>
+            <label for='text' > Mensagem: </label>
+            <textarea id='text' name='mensagem' required></textarea>
+          </div>
+          <div className='submit'>
+
+            <input type='submit' onClick={() => { direction() }} ></input>
+
+          </div>
+
+
+
+        </form>
+      </div>
     );
   }
 }
